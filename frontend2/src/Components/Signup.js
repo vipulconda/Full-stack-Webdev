62cf3293ca16e13304d5f2a40b6c244bf6d42683
Vipul Singh <vipul02vns@gmail.com>
2024-09-08 11:39:59 +0530

    changes

--- frontend2/src/Components/Signup.js
+++ frontend2/src/Components/Signup.js
@@ -35,7 +35,7 @@ const Register = () => {
   const handleverificaton = async (e) => {
     e.preventDefault();
     try {
-      const fullUrl = `${process.env.REACT_APP_API_URL}/${"/send-otp"}`;
+      const fullUrl = `${process.env.REACT_APP_API_URL}${"/send-otp"}`;
       const response = await axios.post(fullUrl, {
         firstname,
         lastname,
